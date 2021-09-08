f6293ba4022380c1fc010f21945219ffd4c20961
gatsby <gatsby@gatsbydeiMac.local>
2021-09-08 22:29:05 +0800

    Initial commit

---
+++ src/main/java/ink/gatsby/taskorder/TaskOrderApplication.java
@@ -0,0 +1,13 @@
+package ink.gatsby.taskorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/gatsby/taskorder/TaskOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.gatsby.taskorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
